66efda0d357627e9650bbf2e08e26ab5e7e7e696
simhyeongseop <202002513@o.cnu.ac.kr>
2025-11-02 19:25:42 +0900

    과제 제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,66 @@
 #include <iostream>
 #include "bird.h"
 
+/*
+DUCK CLASS (METHODS)
+- cry() 메서드: "Quak!" 문자열을 출력 (.bat 파일에 따라 수정됨)
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Duck" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/ 
+Duck::Duck() : Bird("Duck") {
+    cry();
+}
+
+void Duck::cry() {
+    std::cout << "Quack!" << std::endl;
+}
+
+Duck::~Duck() {
+    cry();
+}
+
+/*
+PENGUIN CLASS (METHODS)
+- cry() 메서드: "Squawk!" 문자열을 출력 (.bat 파일에 따라 수정됨)
+- fly() 메서드: "This bird can't fly." 문자열을 출력
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Penguin" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/ 
+Penguin::Penguin() : Bird("Penguin") {
+    cry();
+}
+
+void Penguin::cry() {
+    std::cout << "Squawk!" << std::endl;
+}
+
+void Penguin::fly() {
+    std::cout << "This bird can't fly." << std::endl;
+}
+
+Penguin::~Penguin() {
+    cry();
+}
+
+/*
+EAGLE CLASS (METHODS)
+- cry() 메서드: "Screech!" 문자열을 출력
+- swim() 메서드: "This bird can't swim." 문자열을 출력
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Eagle" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/ 
+Eagle::Eagle() : Bird("Eagle") {
+    cry();
+}
+
+void Eagle::cry() {
+    std::cout << "Screech!" << std::endl;
+}
+
+void Eagle::swim() {
+    std::cout << "This bird can't swim." << std::endl;
+}
+
+Eagle::~Eagle() {
+    cry();
+}
--- bird.h
+++ bird.h
@@ -10,10 +10,6 @@ public:
         std::cout << species << " has borned" << std::endl;
     }
 
-    virtual void display() {
-        std::cout << "Species: " << species << std::endl;
-    }
-
     virtual void fly() {
         std::cout << "This bird can fly." << std::endl;
     }
@@ -22,9 +18,58 @@ public:
         std::cout << "This bird can swim." << std::endl;
     }
 
-    virtual void cry() = 0;
+    virtual void display() {
+        std::cout << "Species: " << species << std::endl;
+    }
+
+    virtual void quack(){};
+
+    virtual void cry(){};
 
     virtual ~Bird() {
         std::cout << species << " has died" << std::endl;
      }
+};
+
+/*
+DUCK CLASS (METHODS)
+- cry() 메서드: "Quak!" 문자열을 출력 (.bat 파일에 따라 수정됨)
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Duck" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/ 
+class Duck : public Bird {
+public:
+    Duck();
+    void cry() override;
+    ~Duck() override;
+};
+
+/*
+PENGUIN CLASS (METHODS)
+- cry() 메서드: "Squawk!" 문자열을 출력 (.bat 파일에 따라 수정됨)
+- fly() 메서드: "This bird can't fly." 문자열을 출력
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Penguin" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/  
+class Penguin : public Bird {
+public:
+    Penguin();
+    void cry() override;
+    void fly() override;
+    ~Penguin() override;
+};
+
+/*
+EAGLE CLASS (METHODS)
+- cry() 메서드: "Screech!" 문자열을 출력
+- swim() 메서드: "This bird can't swim." 문자열을 출력
+- 생성자: 부모 클래스의 생성자를 호출하여 species 변수를 "Eagle" 문자열로 설정, cry() 메서드 호출
+- 소멸자: cry() 메서드 호출
+*/  
+class Eagle : public Bird {
+public:
+    Eagle();
+    void cry() override;
+    void swim() override;
+    ~Eagle() override;
 };
